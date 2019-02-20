242d971205560e00b9e6754117f71830ac2ad894
Adriana Lozano <adriana.lozano@uabc.edu.mx>
2019-02-20 12:16:22 -0800

    cambio menor

--- src/main/java/com/lsc/entity/User.java
+++ src/main/java/com/lsc/entity/User.java
@@ -5,67 +5,67 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-@Table ( name = "user" )
+@Table(name="user")
 public class User {
 
     @Id
-    @Column ( name = "username", nullable = false, unique = true, length = 45 )
+    @Column(name="username", nullable=false, unique=true, length=45)
     private String username;
 
-    @Column ( name = "password", nullable = false, length = 60 )
+    @Column(name="password", nullable=false, length=60)
     private String password;
 
-    @Column ( name = "enabled", nullable = false )
+    @Column(name="enabled", nullable=false)
     private boolean enabled;
 
-    @OneToMany ( fetch = FetchType.EAGER, mappedBy = "user" )
-    private Set < UserRole > userRole = new HashSet < UserRole > ( );
+    @OneToMany(fetch=FetchType.EAGER, mappedBy="user")
+    private Set < UserRole > userRole=new HashSet < UserRole > ();
 
-    public User ( String username , String password , boolean enabled ) {
-        this.username = username;
-        this.password = password;
-        this.enabled = enabled;
+    public User(String username , String password , boolean enabled) {
+        this.username=username;
+        this.password=password;
+        this.enabled=enabled;
     }
 
-    public User ( String username , String password , boolean enabled , Set < UserRole > userRole ) {
-        this.username = username;
-        this.password = password;
-        this.enabled = enabled;
-        this.userRole = userRole;
+    public User(String username , String password , boolean enabled , Set < UserRole > userRole) {
+        this.username=username;
+        this.password=password;
+        this.enabled=enabled;
+        this.userRole=userRole;
     }
 
-    public User ( ) {
+    public User() {
     }
 
-    public String getUsername ( ) {
+    public String getUsername() {
         return username;
     }
 
-    public void setUsername ( String username ) {
-        this.username = username;
+    public void setUsername(String username) {
+        this.username=username;
     }
 
-    public String getPassword ( ) {
+    public String getPassword() {
         return password;
     }
 
-    public void setPassword ( String password ) {
-        this.password = password;
+    public void setPassword(String password) {
+        this.password=password;
     }
 
-    public boolean isEnabled ( ) {
+    public boolean isEnabled() {
         return enabled;
     }
 
-    public void setEnabled ( boolean enabled ) {
-        this.enabled = enabled;
+    public void setEnabled(boolean enabled) {
+        this.enabled=enabled;
     }
 
-    public Set < UserRole > getUserRole ( ) {
+    public Set < UserRole > getUserRole() {
         return userRole;
     }
 
-    public void setUserRole ( Set < UserRole > userRole ) {
-        this.userRole = userRole;
+    public void setUserRole(Set < UserRole > userRole) {
+        this.userRole=userRole;
     }
 }
