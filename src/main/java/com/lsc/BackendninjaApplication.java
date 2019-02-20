48ba1c799a04b2fab70b28a32e54f8b5ad57ed05
Adriana Lozano <adriana.lozano@uabc.edu.mx>
2019-02-20 12:24:26 -0800

    changes to create

--- src/main/java/com/lsc/BackendninjaApplication.java
+++ src/main/java/com/lsc/BackendninjaApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableConfigurationProperties
 @SpringBootApplication
 public class BackendninjaApplication {
 
